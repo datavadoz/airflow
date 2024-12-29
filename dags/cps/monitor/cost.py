850b98b4d26b5673ac3b3b464f5f3fe1d1c33215
Danh Vo <danhvo.uit@gmail.com>
2024-12-30 00:32:46 +0700

    dag(monitor_cost): Migrate Googe cost monitoring

--- dags/cps/monitor/cost.py
+++ dags/cps/monitor/cost.py
@@ -50,15 +50,28 @@ default_args = {
 }
 
 
-def create_external_table():
-    bq = BigQuery('default_bigquery')
-    bq.create_bq_table_from_gsheet_table(
-        gsheet_table=GSheetTable(
+def create_external_table(**kwargs):
+    channel = kwargs.get('channel')
+    if channel == 'facebook':
+        gsheet_table = GSheetTable(
             '14zV1me4r6dHQn6c7nBbpW549eumP9OdfVfUq3kH51uQ',
             'FB_Day',
             'cps_gsheet_bot_facebook.json'
-        ),
-        full_table_id='datavadoz-438714.cps_monitor_gsheet.facebook',
+        )
+    elif channel == 'google':
+        gsheet_table = GSheetTable(
+            '14zV1me4r6dHQn6c7nBbpW549eumP9OdfVfUq3kH51uQ',
+            'GG_ADs_Day',
+            'cps_gsheet_bot_google.json'
+        )
+    else:
+        print(f'Unknown channel {channel}')
+        exit(1)
+
+    bq = BigQuery('default_bigquery')
+    bq.create_bq_table_from_gsheet_table(
+        gsheet_table=gsheet_table,
+        full_table_id=f'datavadoz-438714.cps_monitor_gsheet.{channel}',
         recreate_if_exists=True
     )
 
@@ -66,6 +79,8 @@ def create_external_table():
 def create_partitioned_table(**kwargs):
     execution_date = kwargs.get('execution_date')
     execution_date = execution_date.strftime('%Y-%m-%d')
+    channel = kwargs.get('channel')
+    full_table_id = f'datavadoz-438714.cps_monitor_gsheet.{channel}'
 
     bq = BigQuery('default_bigquery')
 
@@ -74,7 +89,7 @@ def create_partitioned_table(**kwargs):
         sql_template = f.read()
 
     sql_stmt = sql_template.format(
-        full_table_id='datavadoz-438714.cps_monitor_gsheet.facebook'
+        full_table_id=full_table_id
     )
 
     retries = 100
@@ -96,21 +111,25 @@ def create_partitioned_table(**kwargs):
         time.sleep(30)
 
     bq.create_partitioned_table(
-        full_table_id='datavadoz-438714.cps_monitor_gsheet.facebook',
+        full_table_id=full_table_id,
         partitioned_column='date'
     )
 
 
 def gen_report(**kwargs):
-    dmc3_list = kwargs.get('dmc3')
+    channel = kwargs.get('channel')
     execution_date = kwargs.get('execution_date')
     previous_date = execution_date - timedelta(days=1)
     execution_date = execution_date.strftime('%Y-%m-%d')
     previous_date = previous_date.strftime('%Y-%m-%d')
 
+    if channel == 'google':
+        print(f'Unimplemented for {channel}')
+        return
+
     bq = BigQuery('default_bigquery')
 
-    for dmc3 in dmc3_list:
+    for dmc3 in DMC3_LIST:
         dmc3_condition = f"AND dmc3 = '{dmc3}'"
         if dmc3.upper() == 'ALL':
             dmc3_condition = ''
@@ -162,7 +181,7 @@ def gen_report(**kwargs):
 
         TelegramOperator(
             task_id='not_important',
-            telegram_conn_id='tlg_prod',
+            telegram_conn_id='tlg_prod_facebook',
             telegram_kwargs={'parse_mode': 'Markdown'},
             text=msg,
         ).execute(kwargs)
@@ -178,18 +197,39 @@ with DAG(
 
     t002 = PythonOperator(
         task_id='create_external_table_facebook',
-        python_callable=create_external_table
+        python_callable=create_external_table,
+        op_kwargs={'channel': 'facebook'}
     )
 
     t003 = PythonOperator(
         task_id='create_partitioned_table_facebook',
-        python_callable=create_partitioned_table
+        python_callable=create_partitioned_table,
+        op_kwargs={'channel': 'facebook'}
     )
 
     t004 = PythonOperator(
-        task_id='generate_dmc3_report',
+        task_id='generate_report_facebook',
         python_callable=gen_report,
-        op_kwargs={'dmc3': DMC3_LIST}
+        op_kwargs={'channel': 'facebook'}
+    )
+
+    t102 = PythonOperator(
+        task_id='create_external_table_google',
+        python_callable=create_external_table,
+        op_kwargs={'channel': 'google'}
+    )
+
+    t103 = PythonOperator(
+        task_id='create_partitioned_table_google',
+        python_callable=create_partitioned_table,
+        op_kwargs={'channel': 'google'}
+    )
+
+    t104 = PythonOperator(
+        task_id='generate_report_google',
+        python_callable=gen_report,
+        op_kwargs={'channel': 'google'}
     )
 
     t001 >> t002 >> t003 >> t004 >> t999
+    t001 >> t102 >> t103 >> t104 >> t999
